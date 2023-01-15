724758e6660d791fc1c6401bdb4deb4eaa720f33
Nomik94 <kill0374@naver.com>
2023-01-15 18:43:39 +0900

    회원가입 라우팅 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,41 @@
+'use strict';
+
+class UserStorage {
+  static #users = {
+    id: ['jungmin', '나개발', '김팀장'],
+    password: ['1234', '1234', '1234'],
+    name: ['정민', '나개발', '김팀장'],
+  };
+
+  static getUsers(...fields) {
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKey = Object.keys(users);
+    const userInfo = usersKey.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
+  }
+
+  static save(userInfo) {
+    const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.password.push(userInfo.password);
+    return { success: true };
+  }
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 const ctrl = require('./home.ctrl');
 
-router.get('/', ctrl.hello);
+router.get('/', ctrl.output.hello);
 
-router.get('/login', ctrl.login);
+router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
+
+router.post('/login', ctrl.process.login);
+router.post('/register', ctrl.process.register);
 
 module.exports = router;
